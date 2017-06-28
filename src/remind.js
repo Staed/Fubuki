0d98dbc7aeaedfaac2cf1429b6d9e7ac4580aafe
Kelsey <ky.staedler@gmail.com>
2017-06-28 09:23:00 -0400

    Fixed RegExp in remind.js where backspaces were deleted

--- src/remind.js
+++ src/remind.js
@@ -3,20 +3,21 @@
  */
 function remindMe(message) {
   let remindRegex = new RegExp([
-    '^(!remindme)[\s\S]+?(in)[\s]+(\d+[\s]+(hour(s)?|minute(s)?))?',
-    '([\s](and)[\s]+)?(\d+[\s](hour(s)?|minute(s)?))?[\s]*$',
+    '.+?in[\\s]+(\\d+\[\\s]+(hour(s)?|minute(s)?))?',
+    '( and[\\s]+(\\d+[\\s](hour(s)?|minute(s)?)))?',
   ].join(''), 'ig');
 
   if (!remindRegex.test(message.content)) {
     message.channel.sendMessage(
       'That was the wrong format, please send it as such:\n' +
-      '!remind me [Your reminder text] in [Number] [hour(s) or minute(s)]' +
+      '!remindme [Your reminder text] in [Number] [hour(s) or minute(s)]' +
       ' and [Number] [minute(s) or hour(s)]\nYou can omit "and" and' +
       ' everything after it if desired. Case does not matter.'
     )
       .catch( (reason) => {
         console.log('Rejected Remind WrongFormat Promise for ' + reason);
       });
+    return;
   }
   let matchedStrings =
       message.content.match(/\d+[\s]+((hour(s)?)|(minute(s)?))/ig);
@@ -39,8 +40,8 @@ function remindMe(message) {
   }
 
   let remindInput = new RegExp([
-    '([\s]+in[\s]+\d+[\s]+(hour(s)?|minute(s)?))',
-    '([\s](and)[\s]+)?(\d+[\s](hour(s)?|minute(s)?))?[\s]*$',
+    '([\\s]+in[\\s]+\d+[\\s]+(hour(s)?|minute(s)?))',
+    '([\\s](and)[\\s]+)?(\\d+[\\s](hour(s)?|minute(s)?))?[\\s]*$',
   ].join(''), 'ig');
   let remindText = message.content.replace(remindInput, '');
   remindText = remindText.split(' ').slice(1).join(' ');
